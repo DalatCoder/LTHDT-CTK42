df124a4f81c553bb3c4eee096b42cddf2a591928
DalatCoder <hieuntctk42@gmail.com>
2020-04-25 10:58:55 +0700

    Init commit 🚀

---
+++ Lab04-KeThua-StackQueue/SQ_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQ_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

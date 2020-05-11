bf7a9ce55de292c3e152a04a5fa572fb958c7b8a
DalatCoder <hieuntctk42@gmail.com>
2020-05-11 14:56:01 +0700

    Bo sung bai1 lab8

---
+++ Bai1_Lab08/NhiPhan.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Bai1_Lab08
+{
+    public class NhiPhan
+    {
+        private int[] bin;
+        private static int soBit = 8;
+
+        public int[] Bin { get => bin; }
+        public int GetDecimal()
+        {
+                int dec = 0;
+                for (int i = soBit - 2; i >= 0; i--)
+                    dec += (int)(bin[i] * Math.Pow(2, i));
+
+                if (bin[soBit - 1] == 1) return -dec;
+                return dec;
+        }
+
+        public NhiPhan(int num)
+        {
+            bin = new int[soBit];
+            bin = Parse(num).Cast<int>().ToArray();
+        }
+
+        public static int[] Parse(int dec)
+        {
+            var bin = new int[soBit];
+            var stack = new Stack();
+
+            if (dec < 0)
+            {
+                bin[soBit - 1] = 1;
+                dec = Math.Abs(dec);
+            }
+
+            while (dec != 0)
+            {
+                stack.Push(dec % 2);
+                dec /= 2;
+            }
+
+            var count = stack.Count;
+            if (count >= soBit)
+                throw new ArgumentException("So thap phan qua lon!", nameof(dec));
+
+            for (int i = count - 1; i >= 0; i--)
+            {
+                bin[i] = (int)stack.Pop();
+            }
+
+            return bin;
+        }
+
+        public static NhiPhan operator +(NhiPhan a, NhiPhan b)
+        {
+            var dec1 = a.GetDecimal();
+            var dec2 = b.GetDecimal();
+
+            var sum = dec1 + dec2;
+
+            if (sum < -127 || sum > 127) throw new InvalidOperationException("Tran so nhi phan!");
+            return new NhiPhan(sum);
+        }
+
+        public static NhiPhan operator -(NhiPhan a, NhiPhan b)
+        {
+            var dec1 = a.GetDecimal();
+            var dec2 = b.GetDecimal();
+
+            var sub = dec1 - dec2;
+
+            if (sub < -127 || sub > 127) throw new InvalidOperationException("Tran so nhi phan!");
+            return new NhiPhan(sub);
+        }
+
+        public static bool operator ==(NhiPhan a, NhiPhan b)
+        {
+            var isEqual = true;
+            for (int i = 0; i < soBit; i++)
+                if (a.bin[i] != b.bin[i])
+                    isEqual = false;
+            return isEqual;
+        }
+        public static bool operator !=(NhiPhan a, NhiPhan b)
+          => !(a == b);
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            for (int i = soBit - 1; i >= 0; i--)
+                builder.Append(bin[i]);
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Bai1_Lab08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bai1_Lab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Nhap vao 1 so thap phan: ");
+                var dec1 = Convert.ToInt32(Console.ReadLine());
+                var bin1 = new NhiPhan(dec1);
+                Console.WriteLine("Chuoi nhi phan tuong ung: ");
+                Console.WriteLine(bin1);
+
+                Console.WriteLine("Nhap vao so thap phan thu hai: ");
+                var dec2 = Convert.ToInt32(Console.ReadLine());
+                var bin2 = new NhiPhan(dec2);
+                Console.WriteLine("Chuoi nhi phan tuong ung: ");
+                Console.WriteLine(bin2);
+
+                var sum = bin1 + bin2;
+                Console.WriteLine($"{bin1} + {bin2} = {sum}");
+                Console.WriteLine($"Gia tri thap phan tuong ung: {sum.GetDecimal()}");
+
+                var sub = bin1 - bin2;
+                Console.WriteLine($"{bin1} - {bin2} = {bin1 - bin2}");
+                Console.WriteLine($"Gia tri thap phan tuong ung: {sub.GetDecimal()}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Co loi xay ra! {0}", ex.Message);
+            }
+        }
+    }
+}
